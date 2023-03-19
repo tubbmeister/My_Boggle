524a124925e6e15e5a765977e7cddea5b0721419
tubbmeister <richard.hoskison@btinternet.com>
2023-03-19 16:24:29 +0000

    Third with portrait only and start of random plan

--- app/src/main/java/com/example/myboggle/MainActivity.java
+++ app/src/main/java/com/example/myboggle/MainActivity.java
@@ -9,22 +9,27 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-   String[] Display_Angle,Top_1;
+   String[] Display_Angle,Top_1,Sel_Letter;
     String Angle,Tile;
     ImageView imageview;
-    TextView text_13;
-    int[] Sel_Image;//array of int
+    Integer Cube_Number;
+    TextView text_13,text_1;
+    int[] Sel_Cube;//array of int
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         imageview=findViewById(R.id.Second_Cube);
+        text_1=findViewById(R.id.Cube_1);
         text_13=findViewById(R.id.Cube_13);
         //Display_Angle=R.getStringArray(R.array.Display_Angle);
     }
@@ -44,12 +49,99 @@ float tempFloat = Float.parseFloat(Angle);
 //Tile=Sel_Image[randomNum6] ; //chooses from 6 options
        // Tile="R.drawable.a1";
       //  int int1 = Integer.valueOf(Tile.getText().toString());
+
+        Sel_Letter=r.getStringArray(R.array.nineth_cube); //get image filename from the array
 int choice = (int)(Math.random()*Tiles.length());
 imageview.setImageResource(Tiles.getResourceId(choice,R.drawable.a1));
 imageview.setRotation(tempFloat);
 
-text_13.setText(" C");
+//text_13.setText(" C");
+Tile=Sel_Letter[randomNum6];
+        text_13.setText(Tile);
 text_13.setRotation(tempFloat);
+        List<Integer> list=new ArrayList<Integer>();
+        for(int i=1;i<17;i++)list.add(i);
+        Collections.shuffle(list);
+ Cube_Number= list.get(0);
+        if (Cube_Number==1){
+            Sel_Letter=r.getStringArray(R.array.first_cube);
+            Tile=Sel_Letter[randomNum6];
+            text_13.setText(Tile);
+            Angle=Top_1[randomNum4]; //chooses from 4 options
+             tempFloat = Float.parseFloat(Angle);
+            text_1.setRotation(tempFloat);
+        }
+        else if (Cube_Number==2){
+        Sel_Letter=r.getStringArray(R.array.first_cube);
+        Tile=Sel_Letter[randomNum6];
+        text_2.setText(Tile);
+        Angle=Top_1[randomNum4]; //chooses from 4 options
+        tempFloat = Float.parseFloat(Angle);
+        text_1.setRotation(tempFloat);
+    }else if (Cube_Number==3){
+            Sel_Letter=r.getStringArray(R.array.first_cube);
+            Tile=Sel_Letter[randomNum6];
+            text_13.setText(Tile);
+            Angle=Top_1[randomNum4]; //chooses from 4 options
+            tempFloat = Float.parseFloat(Angle);
+            text_1.setRotation(tempFloat);
+        }else if (Cube_Number==2){
+            Sel_Letter=r.getStringArray(R.array.first_cube);
+            Tile=Sel_Letter[randomNum6];
+            text_13.setText(Tile);
+            Angle=Top_1[randomNum4]; //chooses from 4 options
+            tempFloat = Float.parseFloat(Angle);
+            text_1.setRotation(tempFloat);
+        }else if (Cube_Number==2){
+            Sel_Letter=r.getStringArray(R.array.first_cube);
+            Tile=Sel_Letter[randomNum6];
+            text_13.setText(Tile);
+            Angle=Top_1[randomNum4]; //chooses from 4 options
+            tempFloat = Float.parseFloat(Angle);
+            text_1.setRotation(tempFloat);
+        }else if (Cube_Number==2){
+            Sel_Letter=r.getStringArray(R.array.first_cube);
+            Tile=Sel_Letter[randomNum6];
+            text_13.setText(Tile);
+            Angle=Top_1[randomNum4]; //chooses from 4 options
+            tempFloat = Float.parseFloat(Angle);
+            text_1.setRotation(tempFloat);
+        }else if (Cube_Number==2){
+            Sel_Letter=r.getStringArray(R.array.first_cube);
+            Tile=Sel_Letter[randomNum6];
+            text_13.setText(Tile);
+            Angle=Top_1[randomNum4]; //chooses from 4 options
+            tempFloat = Float.parseFloat(Angle);
+            text_1.setRotation(tempFloat);
+        }else if (Cube_Number==2){
+            Sel_Letter=r.getStringArray(R.array.first_cube);
+            Tile=Sel_Letter[randomNum6];
+            text_13.setText(Tile);
+            Angle=Top_1[randomNum4]; //chooses from 4 options
+            tempFloat = Float.parseFloat(Angle);
+            text_1.setRotation(tempFloat);
+        }else if (Cube_Number==2){
+            Sel_Letter=r.getStringArray(R.array.first_cube);
+            Tile=Sel_Letter[randomNum6];
+            text_13.setText(Tile);
+            Angle=Top_1[randomNum4]; //chooses from 4 options
+            tempFloat = Float.parseFloat(Angle);
+            text_1.setRotation(tempFloat);
+        }else if (Cube_Number==2){
+            Sel_Letter=r.getStringArray(R.array.first_cube);
+            Tile=Sel_Letter[randomNum6];
+            text_13.setText(Tile);
+            Angle=Top_1[randomNum4]; //chooses from 4 options
+            tempFloat = Float.parseFloat(Angle);
+            text_1.setRotation(tempFloat);
+        }else if (Cube_Number==2){
+            Sel_Letter=r.getStringArray(R.array.first_cube);
+            Tile=Sel_Letter[randomNum6];
+            text_13.setText(Tile);
+            Angle=Top_1[randomNum4]; //chooses from 4 options
+            tempFloat = Float.parseFloat(Angle);
+            text_1.setRotation(tempFloat);
+        }
     }
 public void Stop(View view){
         System.exit(0);
